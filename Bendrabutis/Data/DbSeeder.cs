a1c715ce84fc95f1600c0ff566f3d92d8b5e17d5
Antanas Babič <antbab@ktu.lt>
2022-11-06 17:06:38 +0200

    Update Seeder

--- Bendrabutis/Data/DbSeeder.cs
+++ Bendrabutis/Data/DbSeeder.cs
@@ -19,44 +19,73 @@ namespace Bendrabutis.Data
         public async Task SeedAsync()
         {
             await AddDefaultRoles();
-            await AddOwnerUser();
-            await AddAdminUser();
+            await AddUsers();
         }
 
-        private async Task AddOwnerUser()
+        private async Task AddUsers()
         {
             var ownerUser = new User()
             {
                 UserName = "owner",
                 Email = "owner@ktu.lt",
             };
-
-            var existingUser = await _userManager.FindByEmailAsync(ownerUser.Email);
-            if (existingUser == null)
-            {
-                var createdAdminUser = await _userManager.CreateAsync(ownerUser, "Owner123");
-                if (createdAdminUser.Succeeded)
-                {
-                    await _userManager.AddToRoleAsync(ownerUser, DormitoryRoles.Owner);
-                }
-            }
-        }
-
-        private async Task AddAdminUser()
-        {
             var adminUser = new User()
             {
                 UserName = "admin",
                 Email = "admin@ktu.lt",
             };
+            var visitorUser = new User()
+            {
+                UserName = "visitor",
+                Email = "visitor@ktu.lt",
+            };
+            var residentUser = new User()
+            {
+                UserName = "resident",
+                Email = "resident@ktu.lt",
+            };
 
-            var existingUser = await _userManager.FindByEmailAsync(adminUser.Email);
+            // Owner.
+            var existingUser = await _userManager.FindByEmailAsync(ownerUser.Email);
+            if (existingUser == null)
+            {
+                var createdOwnerUser = await _userManager.CreateAsync(ownerUser, "Owner123");
+                if (createdOwnerUser.Succeeded)
+                {
+                    await _userManager.AddToRoleAsync(ownerUser, DormitoryRoles.Owner);
+                }
+            }
+
+            // Admin.
+            existingUser = await _userManager.FindByEmailAsync(adminUser.Email);
             if (existingUser == null)
             {
                 var createdAdminUser = await _userManager.CreateAsync(adminUser, "Admin123");
                 if (createdAdminUser.Succeeded)
                 {
                     await _userManager.AddToRoleAsync(adminUser, DormitoryRoles.Admin);
+                }
+            }
+
+            // Visitor.
+            existingUser = await _userManager.FindByEmailAsync(visitorUser.Email);
+            if (existingUser == null)
+            {
+                var createdVisitorUser = await _userManager.CreateAsync(visitorUser, "Visitor123");
+                if (createdVisitorUser.Succeeded)
+                {
+                    await _userManager.AddToRoleAsync(visitorUser, DormitoryRoles.Visitor);
+                }
+            }
+
+            // Resident.
+            existingUser = await _userManager.FindByEmailAsync(residentUser.Email);
+            if (existingUser == null)
+            {
+                var createdResidentUser = await _userManager.CreateAsync(residentUser, "Resident123");
+                if (createdResidentUser.Succeeded)
+                {
+                    await _userManager.AddToRoleAsync(residentUser, DormitoryRoles.Resident);
                 }
             }
         }
